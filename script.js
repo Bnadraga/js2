a24027f88268437a23e5288085b1ef8e38a133c5
Bnadraga <player.gamer1mw@gmail.com>
2024-10-18 20:15:31 +0300

    lesson 2

--- script.js
+++ script.js
@@ -35,7 +35,45 @@
 //     alert('des message')
 // })
 
-const box = document.querySelector("div.box");
-box.addEventListener("click", function (event) {
-    console.log(event.target)
-});
+// const box = document.querySelector("div.box");
+// box.addEventListener("click", function (event) {
+//     console.log(event.target)
+// });
+
+const output = document.querySelector(".output");
+const colorPalette = document.querySelector(".color-palette");
+
+colorPalette.addEventListener("click", selectColor);
+function selectColor(ev) {
+  if (ev.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  const selectedColor = ev.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+  output.style.color = selectedColor;
+}
+
+createPaletteItems();
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRandomHexColor();
+    const item = document.createElement("button");
+    item.type = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add("item");
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+function getRandomHexColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
