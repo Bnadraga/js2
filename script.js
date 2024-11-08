84b56cd5f2b453c13d563b6bae77cb9a6a575688
Bnadraga <player.gamer1mw@gmail.com>
2024-11-08 20:35:30 +0200

    lesson

--- script.js
+++ script.js
@@ -228,13 +228,52 @@
 
 // IntersectionObserver(callback, option);
 
-const options = {
-  rootMargin: "50px",
-  threshold: 0.25,
-};
-const onEntry = (entries, observer) => {
-  entries.forEach((entry) => {
-    // ...
+// const options = {
+//   rootMargin: "50px",
+//   threshold: 0.25,
+// };
+// const onEntry = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     // ...
+//   });
+// };
+// const observer = new IntersectionObserver(onEntry, options);
+
+// const options = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.1,
+// };
+// const observer = new IntersectionObserver((entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       const img = entry.target;
+//       img.src = img.dataset.src;
+//       observer.unobserve();
+//     }
+//   });
+// }, options);
+
+// document.querySelectorAll(".lazy-img").forEach((img) => {
+//   observer.observe(img);
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fruits = document.querySelectorAll(".fruits-list li");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const img = entry.target.querySelector("img");
+      const title = entry.target.querySelector("h3");
+
+      if (entry.isIntersecting) {
+        img.src = img.getAttribute("data-high-res");
+        entry.target.classList.add("visible");
+      } else {
+        img.src = img.src.replace("-1", "-2");
+        entry.target.classList.remove("visible");
+      }
+    });
   });
-};
-const observer = new IntersectionObserver(onEntry, options);
+  fruits.forEach((fruit) => observer.observe(fruit));
+});
